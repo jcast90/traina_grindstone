0616dd677be1cef3d55584bb08d792322c5dad2a
Jonathan Lancaster <jonathan@trainadesign.com>
2019-05-20 09:34:42 -0700

    first commit

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class CardContainer extends Component {
+    render(){
+        return(
+        <div className="fillerbox">
+          <h1 className="white-text"> Test </h1>
+          <div className="fillerbox"></div>
+          <div className="fillerbox"></div>
+          <div className="fillerbox"></div>
+        <div className="fillerbox"></div></div>
+        )
+    }
+}
+
+export default CardContainer;
