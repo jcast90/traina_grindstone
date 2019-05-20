ecb0ef3806d3f540cddcac885ad9a886bc68c45d
Jonathan Lancaster <jonathan@trainadesign.com>
2019-05-20 09:40:09 -0700

    Updates

---
+++ precache-manifest.743295d795903c333e81bfa7e48f8b66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc4653b09c585df64dfa8f09be8fd94",
+    "url": "/traina_grindstone/index.html"
+  },
+  {
+    "revision": "185d4650e60c2886fd4a",
+    "url": "/traina_grindstone/static/css/main.f6a6376f.chunk.css"
+  },
+  {
+    "revision": "c9bf8ea0e0538d2da265",
+    "url": "/traina_grindstone/static/js/2.50f842ac.chunk.js"
+  },
+  {
+    "revision": "185d4650e60c2886fd4a",
+    "url": "/traina_grindstone/static/js/main.12a79425.chunk.js"
+  },
+  {
+    "revision": "5613820938561ce98712",
+    "url": "/traina_grindstone/static/js/runtime~main.136ae2f7.js"
+  },
+  {
+    "revision": "834adf7255333678d7326c72633ce5b2",
+    "url": "/traina_grindstone/static/media/logo.834adf72.svg"
+  }
+]);
